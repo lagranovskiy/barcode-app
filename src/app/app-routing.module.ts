17703f20b0c3ac1282a77a9e5a2d46c3f263546d
Leonid Agranovskiy <leonid.agranovskiy@dwpbank.de>
2022-06-18 00:07:20 +0200

    json eingelesen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
+import { SauberUnsauberComponent } from './views/sauber-unsauber/sauber-unsauber.component';
+import { FruechteDesGeistesComponent } from './views/fruechte-des-geistes/fruechte-des-geistes.component';
 import { GameSelectorComponent } from './views/game-selector/game-selector.component';
-import { GameStatisticsComponent } from './views/game-statistics/game-statistics.component';
+import { GameStatisticsComponent } from './components/game-statistics/game-statistics.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'start', component: GameSelectorComponent },
-  { path: 'statistics', component: GameStatisticsComponent },
-  { path: '**', component: GameStatisticsComponent }
+  { path: 'fruechte-des-geistes', component: FruechteDesGeistesComponent },
+  { path: 'sauber-unsauber', component: SauberUnsauberComponent },
+  { path: '**', component: GameSelectorComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { GameSelectorComponent } from './views/game-selector/game-selector.component';
-import { GameStatisticsComponent } from './views/game-statistics/game-statistics.component';
+import { GameStatisticsComponent } from './components/game-statistics/game-statistics.component';
+import { FruechteDesGeistesComponent } from './views/fruechte-des-geistes/fruechte-des-geistes.component';
+import { SauberUnsauberComponent } from './views/sauber-unsauber/sauber-unsauber.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     GameSelectorComponent,
-    GameStatisticsComponent
+    GameStatisticsComponent,
+    FruechteDesGeistesComponent,
+    SauberUnsauberComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatGridListModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
